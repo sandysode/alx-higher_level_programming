2816065d36a7e598e6e05c9a8b9ac722e70c9d94
sandysode <sandraoghenessode@gmail.com>
2023-09-11 06:37:04 -0700

    tuples

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-
 def no_c(my_string):
-    copy_str = [x for x in my_string if x != 'c' or x != 'C']
-    return ("".join(copy_str))
+    new_string = ""
+    for idx in range(len(my_string)):
+        if my_string[idx] != "c" and my_string[idx] != "C":
+            new_string += my_string[idx]
+    return new_string
